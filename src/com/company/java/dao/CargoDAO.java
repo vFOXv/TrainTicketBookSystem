87af40a6bd5c856706a41fc44476372d8afb6c0b
ShDen007 <shden@meta.ua>
2020-08-29 21:13:26 +0300

    Create CargoDAO and file Cargo.txt

--- src/com/company/java/dao/CargoDAO.java
+++ src/com/company/java/dao/CargoDAO.java
@@ -1,39 +1,36 @@
 package dao;
 
 import model.Cargo;
-import java.lang.*;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 // Реализовать методы из интерфейса GenericDAO.
 // Данные должны хранится в файле который расположен в дирректории "com.resources.database_test"
 // (для каждой сущности создается свой файл прим. locomotive.txt).
 // Пример метода getEntityById смотрите в классе LocomotiveDAO.
 
-
-public class CargoDAO implements GenericDAO <Cargo> {
+public class CargoDAO implements GenericDAO<Cargo> {
 
     String filePath = "srs/com/resources/database_test/Cargo.txt";
 
     @Override
     public Cargo getEntityById(Long id) {
-
         Cargo cargo = new Cargo();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String read = null;
             while ((read = reader.readLine()) != null) {
                 String[] splitedLine = read.split(",");
-                String firstStr = splitedLine[0];
+                Long firstLong = Long.parseLong(splitedLine[0]);
 
-                if (firstStr.equalsIgnoreCase(firstStr)) {
-                    cargo.setTypeCargo(splitedLine[0]);
-                    cargo.setWeightCargo(Double.parseDouble(splitedLine[1]));
-                    cargo.setVolumeCargo(Double.parseDouble(splitedLine[2]));
-                    cargo.setQuantityCargo(Integer.parseInt(splitedLine[3]));
+                if (firstLong.equals(id)) {
+                    cargo.setId(firstLong);
+                    cargo.setTypeCargo(splitedLine[1]);
+                    cargo.setWeightCargo(Double.parseDouble(splitedLine[2]));
+                    cargo.setVolumeCargo(Double.parseDouble(splitedLine[3]));
+                    cargo.setQuantityCargo(Integer.parseInt(splitedLine[4]));
                 }
             }
         } catch (IOException e) {
@@ -44,7 +41,7 @@ public class CargoDAO implements GenericDAO <Cargo> {
 
     @Override
     public void saveEntity(Cargo entity) {
-        try (BufferedWriter Cargo1 = new BufferedWriter(new FileWriter(filePath, true))) {
+        try (BufferedWriter bufWri = new BufferedWriter(new FileWriter(filePath, true))) {
 
             String[] luggage = new String[]{String.valueOf(entity.getTypeCargo()),
                     String.valueOf(entity.getWeightCargo()),
@@ -53,16 +50,15 @@ public class CargoDAO implements GenericDAO <Cargo> {
 
             for (int i = 0; i < luggage.length; i++) {
                 if (i != luggage.length - 1) {
-                    Cargo1.write(luggage[i]);
-                    Cargo1.write(",");
+                    bufWri.write(luggage[i]);
+                    bufWri.write(",");
                 } else {
-                    Cargo1.write(luggage[i] + "\n");
+                    bufWri.write(luggage[i] + "\n");
                 }
             }
         } catch (IOException e) {
             e.getStackTrace();
         }
-
     }
 
     @Override
@@ -136,18 +132,8 @@ public class CargoDAO implements GenericDAO <Cargo> {
             e.printStackTrace();
         }
         cargoList.forEach(this::saveEntity);
-  // }
-  // }
-  // class Run {
-  //     public static void main(String[] args) {
-  //         CargoDAO cargoDAO = new CargoDAO();
-  //         Scanner scan = new Scanner(System.in);
-  //         Cargo cargo = new Cargo(null,0.0,0.0,0);
-  //         cargo.setTypeCargo(scan.nextLine());
-  //         System.out.println(cargo.getTypeCargo());
-  //         cargo.setWeightCargo(scan.nextDouble());
-  //         System.out.println(cargo.getWeightCargo());
-
-        }
+  }
 }
+
+
 
