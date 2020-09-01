92f57c83046a3e3db14c3710da3fb5b244b8cd55
FOX <sly13fox@gmail.com>
2020-09-01 20:51:18 +0300

    Add class TicketDAO

--- src/com/company/java/Run.java
+++ src/com/company/java/Run.java
@@ -2,6 +2,8 @@ import dao.LocomotiveDAO;
 import dao.PassengerDAO;
 import model.Locomotive;
 import model.Passenger;
+import model.PassengerWagoon;
+import model.Ticket;
 
 public class Run {
     public static void main(String[] args) {
@@ -14,17 +16,21 @@ public class Run {
 //        locomotiveDAO.saveEntity(locomotive2);
 
         // index = list.indexOf(entity); не работает в ПассажиреДао  :(((
-        Passenger passenger1 = new Passenger("FName_1", "SName_1", 1L);
-        Passenger passenger2 = new Passenger("FName_2", "SName_2", 2L);
-        Passenger passenger3 = new Passenger("FName_3", "SName_3", 3L);
-        Passenger passenger3_4 = new Passenger("FName_3/4", "SName_3/4", 3L);
-        PassengerDAO passengerDAO = new PassengerDAO();
-        passengerDAO.saveEntity(passenger1);
-        passengerDAO.saveEntity(passenger2);
-        passengerDAO.saveEntity(passenger3);
-        passengerDAO.removeEntity(passenger3);
-        System.out.println(passengerDAO.getEntityById(1L).toString());
-        passengerDAO.updateEntity(passenger3_4);
+//        Passenger passenger1 = new Passenger("FName_1", "SName_1", 1L);
+//        Passenger passenger2 = new Passenger("FName_2", "SName_2", 2L);
+//        Passenger passenger3 = new Passenger("FName_3", "SName_3", 3L);
+//        Passenger passenger3_4 = new Passenger("FName_3/4", "SName_3/4", 3L);
+//        PassengerDAO passengerDAO = new PassengerDAO();
+//        passengerDAO.saveEntity(passenger1);
+//        passengerDAO.saveEntity(passenger2);
+//        passengerDAO.saveEntity(passenger3);
+//        passengerDAO.removeEntity(passenger3);
+//        System.out.println(passengerDAO.getEntityById(1L).toString());
+//        passengerDAO.updateEntity(passenger3_4);
+
+        Ticket ticket1 = new Ticket(1, PassengerWagoon.TypeWagoon.PLATSKART,"20/10/2020 in 19:20","21/10/2020 in 20:10",true, Ticket.TeaCoffee.COFFEE, Ticket.Luggage.HEAVY_LUGGAGE);
+        Ticket ticket2 = new Ticket(2, PassengerWagoon.TypeWagoon.CW,"18/10/2020 in 18:50","22/10/2020 in 21:50",false, Ticket.TeaCoffee.TEA, Ticket.Luggage.MEDIUM_LUGGAGE);
+        Ticket ticket3 = new Ticket(3, PassengerWagoon.TypeWagoon.KYPE,"21/01/2020 in 17:30","25/01/2020 in 10:20",true, Ticket.TeaCoffee.NO_DRINKS, Ticket.Luggage.NO_LUGGAGE);
 
 
     }
